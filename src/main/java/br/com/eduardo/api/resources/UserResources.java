65eb92de56d35d2d921f448f17dd2bfc7953a701
Eduardo Simas <simas.developer@gmail.com>
2023-04-13 16:29:42 -0300

    class user resource findById

--- src/main/java/br/com/eduardo/api/resources/UserResources.java
+++ src/main/java/br/com/eduardo/api/resources/UserResources.java
@@ -1,6 +1,8 @@
 package br.com.eduardo.api.resources;
 
-import br.com.eduardo.api.domain.User;
+import br.com.eduardo.api.domain.Users;
+import br.com.eduardo.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResources {
 
+    @Autowired
+    private UserService service;
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(
-                new User(1, "Eduardo", "eduardo@eduardo.com.br", "123"));
+    public ResponseEntity<Users> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
 }
